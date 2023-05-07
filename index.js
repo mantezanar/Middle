4748ab2521cc119739d8d74fae00d7b7072feb7a
KatiLF <katialayi@hotmail.com>
2023-05-07 18:44:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const token1 = 'M+Yidu6bWMk9GKkJopL0Sk+ri/RRcBFTF5DmxvbBZaJj+ouXBWzNeSb0qf+rG0Gu
 const JWT_SECRET = 'DEE18F06FAA7F52C346E1569E13F5A85F501D844E5DD1D4DC7CA81A378A1C37A';
 const util = require('util');
 const { log } = require('console');
+const loginToken1 = '';
 //
 app.use(cors());
 app.post('/login', async (req, res) => {
@@ -21,6 +22,7 @@ app.post('/login', async (req, res) => {
     } else {
       data = {aud: decoded.aud, email: decoded.email}
       const loginToken = jwt.sign(data, token1);
+      loginToken1 = loginToken;
       res.json(loginToken);  
     }
     
@@ -30,14 +32,15 @@ app.post('/login', async (req, res) => {
     //Este es el token que se le manda al usuario autenticado
     const decoded = jwt.verify(token, token1);
     if(decoded == 'sesion actual'){
-      console.log(loginToken);
-      if(!loginToken){
+      console.log(loginToken1);
+      if(!loginToken1){
         const no_sesion = 'No';
-        res.json(no_sesion);
+        const sesionToken = jwt.sign(no_sesion, token1);
+        res.json(sesionToken);
       }else {
         data = 'Sesion inciada';      
-      const sesionToken = jwt.sign(data, token1);
-      res.json(loginToken); 
+      const sesionToken = jwt.sign(loginToken1, token1);
+      res.json(sesionToken); 
       }
     } 
     /*data = { }
