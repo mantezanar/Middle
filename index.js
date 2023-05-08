2074fa5295ddb1e6ae510016a9aa4714b5d98967
Mantezanar <mauricio.antezanar@gmail.com>
2023-05-08 14:40:31 -0400

    prueba1

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/files', async (req, res) => {
 
   try {
     const supabase = await connect();
-    const { data: files, error } = await supabase.storage.from('Ejemplo').list(algebra);
+    const { data: files, error } = await supabase.storage.from('Ejemplo').list(Algebra);
 
     if (error) {
       throw error;
