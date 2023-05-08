ffe1f1ab008db9b9ddca6320157d037aea62d086
KatiLF <katialayi@hotmail.com>
2023-05-07 21:46:35 -0400

    cambios

--- index.js
+++ index.js
@@ -4,13 +4,57 @@ const cors = require('cors');
 const supabase = require("./utils/supabase");
 const app = express();
 app.use(express.json());
+//secretToken =
 const token1 = 'M+Yidu6bWMk9GKkJopL0Sk+ri/RRcBFTF5DmxvbBZaJj+ouXBWzNeSb0qf+rG0GuLXqeD34vZ0RKH2LnS+0INw==';
-const JWT_SECRET = 'DEE18F06FAA7F52C346E1569E13F5A85F501D844E5DD1D4DC7CA81A378A1C37A';
+//token2 = 
+const JWT_SECRET = 'DEE18F06FAA7F52C346E1569E13F5A85F501D844E5DD1D4DC7CA81A378A1C37A'; 
 const util = require('util');
 const { log } = require('console');
 
 //
 app.use(cors());
+
+// Ruta protegida
+app.post('/', async (req, res) =>{
+  const supabase = await connect();
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'No se proporcionó el token de autorización' });
+  }
+
+   jwt.verify(token, secretToken, async (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Token inválido' });
+    }
+
+    console.log('Token decodificado:', decoded);
+    const correo = decoded.email;
+    const contrasena = decoded.pass;
+    
+    const result =  await supabase.auth.signInWithPassword({
+        email: correo,
+        password: contrasena
+     });
+    const { user, error } = result;
+     if (error) {
+        const token = jwt.sign("Credenciales no validas", token2);
+        res.json({token});
+        return;
+     } else {
+        const token = jwt.sign(result.data.session.user, token2);
+        res.json({token});
+     }
+
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado en el puerto 3000');
+});
+
+
+/* 
 app.post('/login', async (req, res) => {
   var loginToken;
   try {
@@ -52,12 +96,8 @@ app.post('/login', async (req, res) => {
       }
     } 
     /*data = { }
-    const sesionToken = jwt.sign(data, token1);*/
+    const sesionToken = jwt.sign(data, token1);
     
   }
 });
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+*/
--- utils/supabase.js
+++ utils/supabase.js
@@ -1,7 +1,57 @@
-const { createClient } = require("@supabase/supabase-js");
+const { createClient } = require('@supabase/supabase-js');
+
+// Datos de conexión a la primera base de datos
+const supabaseUrl1 = "https://tzscatlgcubfpvmcykrh.supabase.co";
+const supabaseAnonKey1 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6c2NhdGxnY3ViZnB2bWN5a3JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE2MDQ0NDksImV4cCI6MTk5NzE4MDQ0OX0.X4va-55G7_wn7BFWSCNOQQH6S6mUdT8aJGcsyy8MLhs";
+
+// Datos de conexión a la segunda base de datos
+const supabaseUrl2 = "https://tzscatlgcubfpvmcykrh.supabase.co";
+const supabaseAnonKey2 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6c2NhdGxnY3ViZnB2bWN5a3JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE2MDQ0NDksImV4cCI6MTk5NzE4MDQ0OX0.X4va-55G7_wn7BFWSCNOQQH6S6mUdT8aJGcsyy8MLhs";
+
+async function connect() {
+  try {
+    const supabase1 = createClient(supabaseUrl1, supabaseAnonKey1);
+    const { data, error1 } = await supabase1.from('status').select();
+    console.log('error: %o',error1)
+    console.log('data: %o',data)
+    if (data!=null) {
+      console.log('Conectado a la base de datos 1');
+      return supabase1;
+    }
+  } catch (err) {
+    console.error(`Error al conectar a la base de datos 1: ${err}`);
+  }
+
+  try {
+    const supabase2 = createClient(supabaseUrl2, supabaseAnonKey2);
+    const { data, error2 } = await supabase2.from('status').select();
+    console.log('error: %o',error2)
+    console.log('data: %o',data)
+    if (data!=null) {
+      console.log('Conectado a la base de datos 2');
+      return supabase2;
+    }
+  } catch (err) {
+    console.error(`Error al conectar a la base de datos 2: ${err}`);
+  }
+
+  console.error('No se pudo conectar a ninguna base de datos');
+  return null;
+}
+module.exports = { connect };
+
+
+//export const supabase = () => connect;
+
+// const supabaseClient = await connect();
+// export const supabaseClient = connect;
+
+
+
+/* const { createClient } = require("@supabase/supabase-js");
 
 const SUPABASE_URL = "https://tzscatlgcubfpvmcykrh.supabase.co";
 const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6c2NhdGxnY3ViZnB2bWN5a3JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE2MDQ0NDksImV4cCI6MTk5NzE4MDQ0OX0.X4va-55G7_wn7BFWSCNOQQH6S6mUdT8aJGcsyy8MLhs";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
-module.exports = supabase;
+module.exports = supabase;*/ 
