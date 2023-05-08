0fc468618c857ae8aaf604910c12839a53b640d8
Mantezanar <mauricio.antezanar@gmail.com>
2023-05-08 12:33:22 -0400

    prueba1

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
-import { express } from "express";
-import cookieParser from "cookie-parser";
+const express = require('express');
+const cors = require('cors');
+const {connect} = require('./utils/supabase');
 
-const app = express()
 
-app.use(cookieParser())
+const jwt = require('jsonwebtoken');
+const app = express();
+const secretToken = "M+Yidu6bWMk9GKkJopL0Sk+ri/RRcBFTF5DmxvbBZaJj+ouXBWzNeSb0qf+rG0GuLXqeD34vZ0RKH2LnS+0INw==";
+var respuesta = ''
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) =>{
-  res.cookie('sesion', 'my cookie')
-  res.send('ho la')
-})
+// Ruta protegida
+app.post('/', async (req, res) =>{
+  const supabase = await connect();
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'No se proporcionó el token de autorización' });
+  }
 
-app.listen(3000)
+   jwt.verify(token, secretToken, async (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Token inválido' });
+    }
+
+    console.log('Token decodificado:', decoded);
+    const correo = decoded.email;
+    const contrasena = decoded.pass;
+    
+    const result =  await supabase.auth.signInWithPassword({
+        email: correo,
+        password: contrasena
+     });
+     const { user, error } = result;
+
+     if (error) {
+        const token = jwt.sign("Credenciales no validas", secretToken);
+        res.json(token);
+        return;
+     } else {
+        respuesta = jwt.sign(result.data.session.user.aud, secretToken);
+        res.json(respuesta);
+     }
+  });
+});
+
+
+app.post('/pedirsesion', async (req, res) =>{
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+  const prueba1 = jwt.verify(token, secretToken);
+  res.json(respuesta)
+  respuesta = ''
+});
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado en el puerto 3000');
+});
